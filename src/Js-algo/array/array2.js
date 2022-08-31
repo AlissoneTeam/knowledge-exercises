8f502ffa6e35e68d242eb9a16198db74dfec3bce
Alissone Neyret <alissone.neyret@gmail.com>
2022-08-31 16:55:33 +0200

    fix: update instruction

--- src/Js-algo/array/array2.js
+++ src/Js-algo/array/array2.js
@@ -2,6 +2,6 @@ export const fruits = ['pomme', 'banane', 'fraise', 'cerise']
 
 export const afficheUnElement = (tableauElements) =>  {
     /* 
-    Ecris une instruction qui affiche l'élément "fraise"
+    Ecris une instruction qui affiche le 3ème élément du tableau
     */
 }
--- src/Js-algo/filter/filter1.js
+++ src/Js-algo/filter/filter1.js
@@ -1,5 +1,6 @@
 const ages = [6,12,8,18,35,55]
 
 export const majeur = (tableauAges) => {
+
     /* Ecris des instructions qui permettent de retourner un tableau contenant uniquement les âges supérieurs ou égaux à 18 */
 }
